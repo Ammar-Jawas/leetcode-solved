5c89c37a5fef60fd801d0d68dc6d4e0388fbd39d
Ammar <jawasammar@gmail.com>
2023-12-14 23:34:28 +0700

    Time: 877 ms (8.25%) | Memory: 0B (100.00%) - LeetSync

--- 570-managers-with-at-least-5-direct-reports/managers-with-at-least-5-direct-reports.sql
+++ 570-managers-with-at-least-5-direct-reports/managers-with-at-least-5-direct-reports.sql
@@ -3,7 +3,8 @@ SELECT e.name
 FROM Employee e, Employee m
 WHERE e.id = m.managerId
 GROUP BY m.managerId
-HAVING count(m.managerId) >= 5
+having count(m.managerId) >= 5
+
 
 /*select name from employee where id in
 (Select managerId
