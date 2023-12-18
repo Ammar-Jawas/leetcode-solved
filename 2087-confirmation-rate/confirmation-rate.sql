b4877edf980f0b051383931b5856dfa76a45f146
Ammar <jawasammar@gmail.com>
2023-12-18 23:40:10 +0700

    Time: 1067 ms (95.38%) | Memory: 0B (100.00%) - LeetSync

--- 2087-confirmation-rate/confirmation-rate.sql
+++ 2087-confirmation-rate/confirmation-rate.sql
@@ -1,5 +1,19 @@
 # Write your MySQL query statement below
-SELECT s.user_id, case when c.time_stamp is null then 0.00 else round(sum(c.action = "confirmed") / count(*),2) end as confirmation_rate
+
+/*SELECT s.user_id, case when c.time_stamp is null then 0.00 else round(sum(c.action = "confirmed") / count(*),2) end as confirmation_rate
 FROM Signups s
 LEFT JOIN Confirmations c on s.user_id = c.user_id
-GROUP BY s.user_id
+GROUP BY s.user_id*/
+
+
+select s.user_id, case when c.action is null then 0.00 else round(sum(c.action = "confirmed")/count(*),2) end as confirmation_rate
+from Signups s 
+left join Confirmations c on s.user_id = c.user_id
+group by s.user_id
+
+
+
+
+
+
+
